a8b958cfe3aa226df0b0add0e5343aebfbd3d019
c4seus <c4seus@proton.me>
2025-09-15 21:18:48 +0200

    add some basic test

--- morse/tests.py
+++ morse/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get(reverse("home_view"))
+        self.assertEqual(response.status_code, 200)
